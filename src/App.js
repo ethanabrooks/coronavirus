8e4560c8b9816a42430725bed18a0e6136f86b26
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-24 19:25:51 -0400

    add bars and gridlines

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "../node_modules/react-vis/dist/style.css";
+import {
+  XYPlot,
+  LineSeries,
+  VerticalGridLines,
+  HorizontalGridLines,
+  XAxis,
+  YAxis
+} from "react-vis";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { x: 0, y: 8 },
+  { x: 1, y: 5 },
+  { x: 2, y: 4 },
+  { x: 3, y: 9 },
+  { x: 4, y: 1 },
+  { x: 5, y: 7 },
+  { x: 6, y: 6 },
+  { x: 7, y: 3 },
+  { x: 8, y: 2 },
+  { x: 9, y: 0 }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <XYPlot height={300} width={300}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <LineSeries data={data} />
+        </XYPlot>
+      </div>
+    );
+  }
 }
 
 export default App;
