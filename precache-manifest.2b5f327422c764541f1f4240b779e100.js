5d9d8efbe126ad8b7bd97a6ea6b48612c09edb49
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-25 01:34:04 -0400

    Updates

--- precache-manifest.2b5f327422c764541f1f4240b779e100.js
+++ precache-manifest.2b5f327422c764541f1f4240b779e100.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cdb7d67c8ea3e573302d04e77a88015b",
+    "revision": "601afaf211c0d142340b45097de52b26",
     "url": "/coronavirus/index.html"
   },
   {
-    "revision": "689b3f68d951eb6fdb91",
+    "revision": "c5fe6436876f6172f729",
     "url": "/coronavirus/static/css/2.5a2f7233.chunk.css"
   },
   {
-    "revision": "754dfb290dc489f4422e",
+    "revision": "7cf2b789fa3d72b15377",
     "url": "/coronavirus/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "689b3f68d951eb6fdb91",
-    "url": "/coronavirus/static/js/2.c7d6b1fe.chunk.js"
+    "revision": "c5fe6436876f6172f729",
+    "url": "/coronavirus/static/js/2.bb0ca893.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/coronavirus/static/js/2.c7d6b1fe.chunk.js.LICENSE.txt"
+    "url": "/coronavirus/static/js/2.bb0ca893.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "754dfb290dc489f4422e",
-    "url": "/coronavirus/static/js/main.480b2b43.chunk.js"
+    "revision": "7cf2b789fa3d72b15377",
+    "url": "/coronavirus/static/js/main.49678aae.chunk.js"
   },
   {
     "revision": "d4ed5d8cb64b605a1775",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coronavirus/precache-manifest.3b8e93bd96c84dc3f91ee744be4e6f0a.js"
+  "/coronavirus/precache-manifest.2b5f327422c764541f1f4240b779e100.js"
 );
 
 self.addEventListener('message', (event) => {
