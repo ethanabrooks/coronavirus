a9d93992758c6e3ac6badd5baa8fac029ae6629e
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-24 21:47:45 -0400

    Updates

---
+++ precache-manifest.5e62610c31b1e2b31cbbe09310a31060.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ccbe4e4de87f7ba6c4356e81eba4fcf",
+    "url": "/coronavirus/index.html"
+  },
+  {
+    "revision": "4f8b53c49bc9300cfb02",
+    "url": "/coronavirus/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "5e7bc05d8946d1bca39c",
+    "url": "/coronavirus/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f8b53c49bc9300cfb02",
+    "url": "/coronavirus/static/js/2.3e6ca8e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coronavirus/static/js/2.3e6ca8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7bc05d8946d1bca39c",
+    "url": "/coronavirus/static/js/main.f632ac62.chunk.js"
+  },
+  {
+    "revision": "d4ed5d8cb64b605a1775",
+    "url": "/coronavirus/static/js/runtime-main.eac4a3ef.js"
+  }
+]);
