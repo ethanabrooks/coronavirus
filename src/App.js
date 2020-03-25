065b14520039f06c3f0e564a8449e11dff3f6f3c
Alec Newman <alec@rovaughn.com>
2020-03-24 20:24:55 -0600

    HooksApp

--- src/App.js
+++ src/App.js
@@ -11,6 +11,62 @@ import {
   VerticalBarSeriesCanvas
 } from "react-vis";
 
+function HooksApp() {
+  const [state, setState] = React.useState({
+    error: null,
+    isLoaded: false,
+    useCanvas: null,
+    data: []
+  });
+
+  React.useEffect(() => {
+    fetch("https://covidtracking.com/api/states.json")
+      .then(res => res.json())
+      .then(
+        result => {
+          setState({
+            isLoaded: true,
+            data: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          setState({
+            isLoaded: true,
+            error
+          });
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    const chart = data.map(d => ({ x: d.state, y: d.positive }));
+
+    console.log(chart);
+
+    const BarSeries = state.useCanvas
+      ? VerticalBarSeriesCanvas
+      : VerticalBarSeries;
+    return (
+      <div>
+        <XYPlot xType="ordinal" width={3000} height={300} xDistance={100}>
+          <VerticalGridLines />
+          <HorizontalGridLines />
+          <XAxis />
+          <YAxis />
+          <BarSeries className="vertical-bar-series-example" data={chart} />
+        </XYPlot>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
