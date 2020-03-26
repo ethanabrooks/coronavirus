1d382ea4fd01133b0f9a17af83fd44f79a0f2087
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-26 00:06:09 -0400

    got things working with recharts

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import {
   YAxis,
   Tooltip
 } from "recharts";
-import { Set, Map, List, Collection } from "immutable";
+import { Map, List, Collection } from "immutable";
 
 type Entry = {
   state: string;
@@ -21,6 +21,62 @@ type State =
   | { type: "loading" }
   | { type: "error"; error: any }
   | { type: "loaded"; data: Entry[] };
+
+const colors = [
+  "#ff0079",
+  "#fd1c7d",
+  "#fb2b80",
+  "#fa3584",
+  "#f83e88",
+  "#f6468b",
+  "#f44d8f",
+  "#f25393",
+  "#ef5996",
+  "#ed5f9a",
+  "#eb649e",
+  "#e969a1",
+  "#e66ea5",
+  "#e472a8",
+  "#e177ac",
+  "#df7baf",
+  "#dc7fb2",
+  "#da83b6",
+  "#d787b9",
+  "#d48bbc",
+  "#d18fc0",
+  "#ce92c3",
+  "#cb96c6",
+  "#c89ac9",
+  "#c59dcc",
+  "#c2a1cf",
+  "#bea4d2",
+  "#bba7d5",
+  "#b7abd8",
+  "#b4aeda",
+  "#b0b1dd",
+  "#adb4e0",
+  "#a9b7e2",
+  "#a5bae5",
+  "#a1bde7",
+  "#9dc0e9",
+  "#99c3ec",
+  "#95c6ee",
+  "#90c9f0",
+  "#8cccf2",
+  "#88cff3",
+  "#83d2f5",
+  "#7fd4f7",
+  "#7ad7f8",
+  "#76dafa",
+  "#71ddfb",
+  "#6cdffc",
+  "#68e2fd",
+  "#63e4fe",
+  "#5fe7fe",
+  "#5beaff",
+  "#57ecff",
+  "#53efff"
+];
 
 const App: React.FC<{}> = () => {
   const [state, setState] = React.useState<State>({ type: "loading" });
@@ -42,87 +98,62 @@ const App: React.FC<{}> = () => {
     case "error":
       return <div>Error: {state.error.message}</div>;
     case "loaded":
-      const states: Set<string> = Set(state.data.map((e: Entry) => e.state));
       const dtf = new Intl.DateTimeFormat("en", {
         year: "numeric",
         month: "short",
         day: "2-digit"
       });
-      const dat = List(state.data)
+      const nested_data: Collection.Keyed<
+        Date,
+        Collection.Keyed<string, number>
+      > = List(state.data)
         .groupBy((e: Entry): Date => e.dateChecked)
         .map((entries: Collection<number, Entry>) =>
           entries
             .groupBy((e: Entry): string => e.state)
             .map((entries: Collection<number, Entry>): Entry => entries.first())
             .map((e: Entry) => e.positive)
-        )
+        );
+      const most_recent_data = nested_data.maxBy((_, k) => k);
+      if (typeof most_recent_data === "undefined") {
+        return <div>Error: "Empty data"</div>;
+      }
+      const states: string[] = most_recent_data
+        .sortBy((v, k) => -v)
+        .keySeq()
+        .toArray();
+      console.log(states);
+
+      const data = nested_data
         .entrySeq()
         .map(([date, cases]) =>
-          Map(cases).set("name", new Date(date).valueOf())
-        );
+          Map(cases).set("date", new Date(date).valueOf())
+        )
+        .toList()
 
-      const data = [
-        {
-          name: 1585166400000,
-          uv: 4000,
-          pv: 2400,
-          amt: 2400
-        },
-        {
-          name: "Page B",
-          uv: 3000,
-          pv: 1398,
-          amt: 2210
-        },
-        {
-          name: "Page C",
-          uv: 2000,
-          pv: 9800,
-          amt: 2290
-        },
-        {
-          name: "Page D",
-          uv: 2780,
-          pv: 3908,
-          amt: 2000
-        },
-        {
-          name: "Page E",
-          uv: 1890,
-          pv: 4800,
-          amt: 2181
-        },
-        {
-          name: "Page F",
-          uv: 2390,
-          pv: 3800,
-          amt: 2500
-        },
-        {
-          name: "Page G",
-          uv: 3490,
-          pv: 4300,
-          amt: 2100
-        }
-      ];
+        .sortBy((m: Map<string, number>) => m.get("date"));
 
-      console.log("data start");
-      console.log(dat.toJS());
-      console.log("data end");
-      console.log(data);
       return (
         <div>
-          <h1> hello</h1>
           <LineChart
-            width={600}
-            height={300}
-            data={dat.toJS()}
-            margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+            width={1000}
+            height={600}
+            data={data.toJS()}
+            margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
           >
-            {states.toArray().map((s: string) => {
-              return <Line type="monotone" dataKey={s} stroke="#8884d8" />;
+            {states.map((s: string, i: number) => {
+              return <Line type="monotone" dataKey={s} stroke={colors[i]} />;
             })}
-            <XAxis dataKey="name" />
+            <XAxis
+              dataKey={e => {
+                const [
+                  { value: mo },
+                  { value: da },
+                  { value: ye }
+                ] = dtf.formatToParts(new Date(e.date));
+                return `${mo} ${da} ${ye}`;
+              }}
+            />
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
             <XAxis dataKey="name" />
             <YAxis />
