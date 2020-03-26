f3e8ed13531f09cdb0b2de48fbac1e5f4c32decc
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-26 01:08:01 -0400

    Updates

---
+++ precache-manifest.010a9c8dfd94ef59ed7ba58ee84e601c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f7a2ede8deb87973fd79683a3cf0c6",
+    "url": "/coronavirus/index.html"
+  },
+  {
+    "revision": "6dfcd8c1927c779b191f",
+    "url": "/coronavirus/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "21e7cb6741257b569ef0",
+    "url": "/coronavirus/static/css/main.8199ee72.chunk.css"
+  },
+  {
+    "revision": "6dfcd8c1927c779b191f",
+    "url": "/coronavirus/static/js/2.466f64cc.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/coronavirus/static/js/2.466f64cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e7cb6741257b569ef0",
+    "url": "/coronavirus/static/js/main.5c4d72ca.chunk.js"
+  },
+  {
+    "revision": "d4ed5d8cb64b605a1775",
+    "url": "/coronavirus/static/js/runtime-main.eac4a3ef.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coronavirus/precache-manifest.3f9e8605aee9344b4613d2f547a05dfe.js"
+  "/coronavirus/precache-manifest.010a9c8dfd94ef59ed7ba58ee84e601c.js"
 );
 
 self.addEventListener('message', (event) => {
