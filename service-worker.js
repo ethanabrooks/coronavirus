d64c81cfedf3cb1e1eba036f64c0c90c724424a8
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-26 23:21:59 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coronavirus/precache-manifest.e08bc5551980f0e71e665daa8c6e76e6.js"
+  "/coronavirus/precache-manifest.a7f445b05b1c9348dd3a83759ccef473.js"
 );
 
 self.addEventListener('message', (event) => {
