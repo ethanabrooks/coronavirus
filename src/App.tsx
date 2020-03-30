e2be6eb8dcd73e556dae8f9dc5ae12dfdeb971f0
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-30 12:14:01 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -42,10 +42,7 @@ type State =
   | { type: "error"; error: any }
   | {
       type: "loaded";
-      data: Map<string, Data>;
-      latest_data: Map<string, number>;
-      states: OrderedSet<string>;
-      excluded: Set<string>;
+      data: OrderedMap<string, Data>;
       window_dimensions: { innerWidth: number; innerHeight: number };
       selected: null | XSelection;
       selecting: null | XSelection;
@@ -79,11 +76,15 @@ const App: React.FC<{}> = () => {
       .then((res: Response) => res.json())
       .then(
         (raw_data: UnparsedEntry[]) => {
-          const data: Map<string, Data> = List(raw_data)
+          const data: OrderedMap<string, Data> = List(raw_data)
             .map((e: UnparsedEntry) => {
               return { ...e, dateChecked: new Date(e.dateChecked) };
             })
             .filterNot((e: Entry) => isNaN(+e.dateChecked))
+            .filter(
+              (e: Entry) =>
+                e.state == "PA" || e.state == "MD" || e.state == "MI"
+            )
             .groupBy((e: Entry): string => e.state)
             .map(
               (entries: Collection<number, Entry>): Data =>
@@ -94,7 +95,7 @@ const App: React.FC<{}> = () => {
                       e.positive,
                     ])
                     .values()
-                )
+                ).sort()
               //entries
               //.groupBy((e: Entry): Date => e.dateChecked)
               //.map(
@@ -103,26 +104,14 @@ const App: React.FC<{}> = () => {
               //)
               //.map((e: Entry) => e.positive)
             )
-            .toMap();
-
-          const latest_data: Map<string, number> = OrderedMap(
-            data.map((d) => d.last())
-          );
-
-          const states: OrderedSet<string> = OrderedSet(data.keys());
-          const cases_selected = params.stateId
-            ? latest_data.get(params.stateId, 0)
-            : null;
-          const excluded = cases_selected
-            ? states.filter((s) => latest_data.get(s, 0) > cases_selected)
-            : Set();
-
+            .toOrderedMap()
+            .sortBy((d: Data): number => {
+              const last: number = d.last();
+              return -last;
+            });
           setState({
             type: "loaded",
             data,
-            latest_data,
-            states,
-            excluded: excluded,
             window_dimensions: window,
             selecting: null,
             selected: null,
@@ -139,35 +128,11 @@ const App: React.FC<{}> = () => {
     case "error":
       return <div>Error: {state.error.message}</div>;
     case "loaded":
-      const dtf = new Intl.DateTimeFormat("en", {
-        month: "short",
-        day: "numeric",
-      });
-
       const {
         innerWidth: width,
         innerHeight: height,
       }: { innerWidth: number; innerHeight: number } = window;
 
-      const chart_data = () => {
-        if (state.selected) {
-          const left = state.selected.left;
-          const right = state.selected.right;
-          return state.data.slice(
-            Math.min(left, right),
-            Math.max(left, right) + 1
-          );
-        }
-        return state.data;
-      };
-      const getCases = (s: string): number => state.latest_data.get(s, 0);
-      const sampleData = [
-        { x: 1, y: 2 },
-        { x: 2, y: 3 },
-        { x: 3, y: 5 },
-        { x: 4, y: 4 },
-        { x: 5, y: 7 },
-      ];
       return (
         <div>
           <div className="intro">
@@ -181,7 +146,8 @@ const App: React.FC<{}> = () => {
               </p>
               <p>{`${state.mouseOverMessage}`}</p>
               <p>
-                {state.excluded.isEmpty()
+                {//state.excluded.isEmpty()
+                false
                   ? ""
                   : "Click on state names to add this state and all those with fewer cases back to chart."}
               </p>
@@ -191,83 +157,40 @@ const App: React.FC<{}> = () => {
             <p>source: The Covid Tracking Project</p>
           </div>
           <div className="chart">
-            <VictoryChart
+            <VictoryGroup
               width={width}
               height={height}
               scale={{ x: "time" }}
+              style={{
+                data: {
+                  strokeWidth: 0,
+                  fillOpacity: 0.3,
+                  fill: default_color,
+                },
+              }}
               containerComponent={
                 <VictoryVoronoiContainer
-                  voronoiDimension="x"
-                  labels={({ datum }) => {
-                    console.log(datum);
-                    return datum.l;
-                  }}
-                  labelComponent={
-                    <VictoryTooltip
-                      cornerRadius={0}
-                      flyoutStyle={{ fill: "white" }}
-                    />
-                  }
+                  labelComponent={<VictoryTooltip centerOffset={{ x: -100 }} />}
+                  labels={({ datum }) => datum.l}
                 />
               }
             >
-              <VictoryGroup
-                style={{
-                  data: { strokeWidth: 3, fillOpacity: 0.4 },
-                }}
-              >
-                <VictoryAxis orientation="bottom" />
-                <VictoryAxis dependentAxis orientation="right" />
-                {state.data.entrySeq().map(
-                  ([s, d]: [string, Data]): JSX.Element => {
-                    return (
-                      <VictoryArea
-                        labelComponent={<VictoryTooltip />}
-                        style={{
-                          data: { fill: default_color, strokeWidth: 0 },
-                        }}
-                        data={d
-                          .entrySeq()
-                          .map(([d, c]: [Date, number]) => {
-                            return { x: d, y: c, l: `${s}: ${c}` };
-                          })
-                          .toArray()}
-                        interpolation={"natural"}
-                        events={[
-                          {
-                            target: "data",
-                            eventHandlers: {
-                              onMouseOver: () => {
-                                return {
-                                  mutation: (props) => {
-                                    const stroke =
-                                      props.style && props.style.stroke;
-                                    return stroke === highlight_color
-                                      ? null
-                                      : {
-                                          style: {
-                                            fill: highlight_color,
-                                            opacity: 1,
-                                          },
-                                        };
-                                  },
-                                };
-                              },
-
-                              onMouseOut: () => {
-                                return {
-                                  mutation: () => null,
-                                };
-                              },
-                            },
-                          },
-                        ]}
-                      />
-                    );
-                  }
-                )}
-              </VictoryGroup>
-            </VictoryChart>
+              {state.data.entrySeq().map(
+                ([s, d]: [string, Data]): JSX.Element => {
+                  return (
+                    <VictoryArea
+                      data={d
+                        .entrySeq()
+                        .map(([d, c]: [Date, number]) => {
+                          return { x: d, y: c, l: `${s}: ${c}` };
+                        })
+                        .toArray()}
+                      interpolation={"natural"}
+                    />
+                  );
+                }
+              )}
+            </VictoryGroup>
           </div>
         </div>
       );
