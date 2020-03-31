8e8bb7cfdc203ef9eb9faa58cc48fb1a210571e2
Ethan Brooks <ethanabrooks@gmail.com>
2020-03-31 18:52:04 -0400

    add fetch command

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,12 @@ interface IProps {
   data?: number[];
 }
 
+type Entry = {
+  state: string;
+  positive: number;
+  dateChecked: Date;
+};
+
 /* Component */
 export const MyD3Component = (props: IProps) => {
   /* The useRef Hook creates a variable that "holds on" to a value across rendering
@@ -16,27 +22,31 @@ export const MyD3Component = (props: IProps) => {
        for instance inserting elements into the DOM using D3 */
   useEffect(
     () => {
-      if (props.data && d3Container.current) {
-        const svg = d3.select(d3Container.current);
+      fetch("https://covidtracking.com/api/states/daily")
+        .then((res) => res.json())
+        .then((raw_data: Entry[]) => {
+          if (props.data && d3Container.current) {
+            const svg = d3.select(d3Container.current);
 
-        // Bind D3 data
-        const update = svg.append("g").selectAll("text").data(props.data);
+            // Bind D3 data
+            const update = svg.append("g").selectAll("text").data(props.data);
 
-        // Enter new D3 elements
-        update
-          .enter()
-          .append("text")
-          .attr("x", (d, i) => i * 25)
-          .attr("y", 40)
-          .style("font-size", 24)
-          .text((d: number) => d);
+            // Enter new D3 elements
+            update
+              .enter()
+              .append("text")
+              .attr("x", (d, i) => i * 25)
+              .attr("y", 40)
+              .style("font-size", 24)
+              .text((d: number) => d);
 
-        // Update existing D3 elements
-        update.attr("x", (d, i) => i * 40).text((d: number) => d);
+            // Update existing D3 elements
+            update.attr("x", (d, i) => i * 40).text((d: number) => d);
 
-        // Remove old D3 elements
-        update.exit().remove();
-      }
+            // Remove old D3 elements
+            update.exit().remove();
+          }
+        });
     },
 
     /*
