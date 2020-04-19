4fe6ad620b20f97b1f3180de4b45d9abe92c5d75
Ethan Brooks <ethanabrooks@gmail.com>
2020-04-19 17:49:06 -0400

    gor graph.py to work

--- graph.py
+++ graph.py
@@ -2,55 +2,62 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
+from collections import namedtuple
+
+Parameters = namedtuple("Parameters", "I N tau gamma lam alpha")
+
+slider_max = Parameters(I=1e3, N=1e6, tau=1, gamma=1, lam=50, alpha=1)
+slider_init = Parameters(I=1e3, N=1e6, tau=1, gamma=0, lam=50, alpha=1)
+slider_ypos = np.arange(len(slider_max) + 1) * 0.05
+t = np.arange(0.0, 1.0, 0.001)
+
+
+def ydata(I, N, lam, tau, gamma, alpha):
+    for _ in t:
+        yield I
+        I += N * (1 - np.exp(-I * lam * tau / N)) - gamma * I
+
 
 fig, ax = plt.subplots()
-plt.subplots_adjust(left=0.25, bottom=0.25)
-t = np.arange(0.0, 1.0, 0.001)
-a0 = 5
-f0 = 3
-delta_f = 5.0
-s = a0 * np.sin(2 * np.pi * f0 * t)
+plt.subplots_adjust(left=0.25, bottom=slider_ypos[-1])
+s = list(ydata(**{k: v for k, v in slider_init._asdict().items()}))
 (l,) = plt.plot(t, s, lw=2)
 ax.margins(x=0)
-
 axcolor = "lightgoldenrodyellow"
-axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-axamp = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
 
-sfreq = Slider(axfreq, "Freq", 0.1, 30.0, valinit=f0, valstep=delta_f)
-samp = Slider(axamp, "Amp", 0.1, 10.0, valinit=a0)
+sliders = Parameters(
+    *[
+        Slider(
+            plt.axes([0.25, ypos, 0.65, 0.03], facecolor=axcolor),
+            field,
+            0,
+            smax,
+            valinit=init,
+            valstep=smax / 100,
+        )
+        for field, smax, init, ypos in zip(
+            slider_max._fields, slider_max, slider_init, slider_ypos
+        )
+    ]
+)
 
 
 def update(val):
-    amp = samp.val
-    freq = sfreq.val
-    l.set_ydata(amp * np.sin(2 * np.pi * freq * t))
+    l.set_ydata(list(ydata(**{k: v.val for k, v in sliders._asdict().items()})))
     fig.canvas.draw_idle()
 
 
-sfreq.on_changed(update)
-samp.on_changed(update)
+for slider in sliders:
+    slider.on_changed(update)
 
 resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
 button = Button(resetax, "Reset", color=axcolor, hovercolor="0.975")
 
 
 def reset(event):
-    sfreq.reset()
-    samp.reset()
+    for slider in sliders:
+        slider.reset()
 
 
 button.on_clicked(reset)
-
-rax = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=axcolor)
-radio = RadioButtons(rax, ("red", "blue", "green"), active=0)
-
-
-def colorfunc(label):
-    l.set_color(label)
-    fig.canvas.draw_idle()
-
-
-radio.on_clicked(colorfunc)
-
 plt.show()
